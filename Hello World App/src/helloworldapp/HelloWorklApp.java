5c217e97dd271abe143c3f5abc2edb1c89fad974
Panagiotis Papas <p.papaspir@gmail.com>
2011-11-22 23:06:56 +0200

    helloworld

---
+++ Hello World App/src/helloworldapp/HelloWorklApp.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package helloworldapp;
+
+/**
+ *
+ * @author Dillinger
+ */
+public class HelloWorklApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Hello World!"); // Display the string.
+    }
+
+}
